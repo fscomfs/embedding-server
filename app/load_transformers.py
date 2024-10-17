a95e1c39e2b7b8604358b6668398069024f1bc3a
xiaofeng.zhou@extremevision.com.cn <xiaofeng.zhou@extremevision.com.cn>
2024-10-17 10:04:14 +0800

    batch 向量化

--- app/load_transformers.py
+++ app/load_transformers.py
@@ -1,9 +1,14 @@
 import os
 import sentence_transformers
 
-def load_transformers(models = os.getenv("MODELS", "TencentBAC/Conan-embedding-v1")):
-    transformers = {}
 
+def load_transformers(models=os.getenv("MODELS", "TencentBAC/Conan-embedding-v1")):
+    transformers = {}
+    cuda_device = os.getenv("NVIDIA_VISIBLE_DEVICES", '')
+    device = 'cpu'
+    if cuda_device != '':
+        device = 'cuda'
     for model in models.split(','):
-        transformers['text-embedding-3-large'] = sentence_transformers.SentenceTransformer(model)
+        transformers['text-embedding-3-large'] = sentence_transformers.SentenceTransformer(model_name_or_path=model,
+                                                                                           device=device)
     return transformers
--- app/main.py
+++ app/main.py
@@ -56,4 +56,4 @@ async def embedding(req: EmbeddingRequest, res: Response):
     }
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8082, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
